13fcfeb32944450461e07f0e649b3f03b8313b38
Phil Cryer <phil@cryer.us>
2010-10-20 23:33:38 -0500

    late night update, end to end 01 test now...

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ questions(){
 	echo -n "	- IP or domainname for server: "
 	read remote_server
 
-	echo -n "	- SSH port on server (default SSH port is 22): "
+	echo -n "	- SSH port on server (default is 22): "
 	read port
 	
 	echo -n "	- lipsync username (must exist on the client and server): "
@@ -67,7 +67,8 @@ questions(){
 
 ssh.keygen(){
 	if [ -f '/home/${username}/.ssh/id_dsa' ]; then
-		echo -n "* Existing SSH key found for ${username} backing up..."
+		#echo -n "* Existing SSH key found for ${username} backing up..."
+		echo "* Existing SSH key found for ${username} backing up..."
 		mv /home/${username}/.ssh/id_dsa /home/${username}/.ssh/id_dsa-OLD
 		if [ $? -eq 0 ]; then
 			echo "done"
@@ -76,23 +77,24 @@ ssh.keygen(){
 		fi
 	fi
 	echo -n "* Creating new SSH key for ${username}..."
-	ssh-keygen -N '' -f /home/${username}/.ssh/id_dsa
+	ssh-keygen -q -N '' -f /home/${username}/.ssh/id_dsa
 	if [ $? -eq 0 ]; then
 		echo "done"
 	else
 		echo; echo "	ERROR: there was an error generating the ssh key"; exit 1
 	fi
 	
-	echo "* Transferring ssh key for ${username} to ${remote_server}"; echo -n "	NOTE: you will be prompted to login..."
-	su ${username} -c "ssh-copy-id ${remote_server}"
-# ssh-copy-id ${remote_server}
+	echo "* Transferring ssh key for ${username} to ${remote_server} (login as $username now)..."; 
+	#echo -n "	NOTE: you will be prompted to login..."
+	su ${username} -c "ssh-copy-id ${remote_server}" >> /dev/null
+	# ssh-copy-id ${remote_server}
 	if [ $? -eq 0 ]; then
-		#echo "done"
-		echo ""
+		X=0	#echo "done"
 	else
 		echo; echo "	ERROR: there was an error transferring the ssh key"; exit 1
 	fi
-	echo "* Setting permissions on the ssh key for ${username} on ${remote_server}"; echo -n "NOTE: you should not be prompted to login)..."
+	echo -n "* Setting permissions on the ssh key for ${username} on ${remote_server}..."; 
+	#echo -n "	NOTE: you should not be prompted to login..."
 	su ${username} -c "ssh ${remote_server} 'chmod 700 .ssh'"
 	#ssh ${remote_server} 'chmod 700 .ssh'
 	if [ $? -eq 0 ]; then
@@ -104,7 +106,7 @@ ssh.keygen(){
 
 create.group(){
 	echo -n "* Creating group lipsync..."
-	grep lipsync /etc/group
+	grep lipsync /etc/group >>/dev/null
 	if [ $? -eq 0 ]; then
 		echo; echo "	NOTICE: existing group lipsync found, not creating"
 	else
@@ -126,14 +128,20 @@ create.user(){
 
 build.conf(){
 	echo -n "* Creating lipsyncd.conf.xml for ${username}..."
-	sed 's|/absolute/path/to/source|'$lipsync_dir_local'|g' etc/lipsyncd.conf.xml > /tmp/lipsyncd.conf.xml.01
-	sed 's|USERNAME|'$username'|g' /tmp/lipsyncd.conf.xml.01 /tmp/lipsyncd.conf.xml.02
-	sed 's|PORT|'$port'|g' /tmp/lipsyncd.conf.xml.02 /tmp/lipsyncd.conf.xml.03
-	sed 's|desthost::module/|'$remote_server::$lipsync_dir_remote'|g' /tmp/lipsyncd.conf.xml.03 > /tmp/lipsyncd.conf.xml
+	sed 's|/absolute/path/to/source|'$lipsync_dir_local/'|g' etc/lipsyncd.conf.xml > /tmp/lipsyncd.conf.xml.01
+	sed 's|USERNAME|'$username'|g' /tmp/lipsyncd.conf.xml.01 > /tmp/lipsyncd.conf.xml.02
+	sed 's|PORT|'$port'|g' /tmp/lipsyncd.conf.xml.02 > /tmp/lipsyncd.conf.xml.03
+	sed 's|desthost::module/|'$remote_server:$lipsync_dir_remote/'|g' /tmp/lipsyncd.conf.xml.03 > /tmp/lipsyncd.conf.xml
 	echo "done"
 }
 
 deploy(){
+	echo -n "* Enabling lipsync for $username..."
+	touch /home/$username/.lipsyncd; chown $username:$username /home/$username/.lipsyncd
+	echo "done"
+	echo -n "* Adding new lipsync user to lipsync group..."
+	adduser $username lipsync >> /dev/null
+	echo "done"
 	echo "* Deploying lipsync..."
 	echo -n "	> installing /usr/bin/lipsync..."
 	cp usr/bin/lipsync /usr/bin; chown root:root /usr/bin/lipsync; chmod 755 /usr/bin/lipsync
@@ -142,7 +150,9 @@ deploy(){
 	cp usr/bin/lipsyncd /usr/bin; chown root:root /usr/bin/lipsyncd; chmod 755 /usr/bin/lipsyncd
 	echo "done"
 	echo -n "	> installing /etc/init.d/lipsyncd..."
-	cp etc/init.d/lipsyncd /etc/init.d/; chown root:root /etc/init.d/lipsyncd; chmod 755 /etc/init.d/lipsyncd
+	#cp etc/init.d/lipsyncd /etc/init.d/; chown root:root /etc/init.d/lipsyncd; chmod 755 /etc/init.d/lipsyncd
+	sed 's|LSUSER|'$username'|g' etc/init.d/lipsyncd > /etc/init.d/lipsyncd; chown root:root /etc/init.d/lipsyncd; chmod 755 /etc/init.d/lipsyncd
+
 	echo "done"
 	echo -n "	> installing /etc/lipsyncd.conf.xml..."
 	#cp etc/lipsyncd.conf.xml /etc
@@ -165,6 +175,10 @@ uninstall(){
 	echo -n "	NOTICE: stopping lipsync service..."
 	/etc/init.d/lipsyncd stop >> /dev/null 
 	echo "done"
+	
+	echo -n "	NOTICE: disabling lipsync for user..."
+	rm -f /home/$username/.lipsyncd
+	echo "done"
 
 	echo -n "	NOTICE: removing lipsync user and group..."
 	userdel lipsync
@@ -186,7 +200,7 @@ uninstall(){
 ###############################################################################
 if [ "${1}" = "uninstall" ]; then
 	echo "	ALERT: Uninstall option chosen, all lipsync files and configuration will be purged!"
-	echo -n "	ALERT: To continue press enter to continue, otherwise hit ctrl-c to bail..."
+	echo -n "	ALERT: To continue press enter to continue, otherwise hit ctrl-c now to bail..."
 	read continue
 	uninstall
 	exit 0
