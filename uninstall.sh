83ecf9ccd4f1ecfe77e33fc81605df466e1a3edd
Phil Cryer <phil@cryer.us>
2011-06-17 11:03:23 -0500

    uninstall now removes the lipsync line from user's crontab cleanly

--- uninstall.sh
+++ uninstall.sh
@@ -5,14 +5,19 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
+if [ -e $CONF_FILE ]; then
+	. $CONF_FILE
+fi
+
 echo "lipsync uninstall script"
-#	rm /home/*/.unison/ar* 
 	rm -rf /usr/share/doc/lipsyncd
 	rm /etc/init.d/lipsync*
 	rm /etc/lipsync*
 	rm /usr/local/bin/lipsync
 	unlink /usr/local/bin/lipsyncd
 	rm /usr/local/bin/lipsync-notify
+	crontab -u $username -l | awk '$0!~/lipsync/ { print $0 }' > newcronjob
+	crontab -u $username newcronjob; rm newcronjob
 exit 0
 
 
